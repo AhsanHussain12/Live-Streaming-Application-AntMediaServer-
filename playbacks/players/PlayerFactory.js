4d54d6051b9314dbb97d1fd777be6828fb7a8f48
Ahsan Hussain <ahsan.hussain@vidizmo.com>
2025-06-25 18:07:33 +0500

    bugs for dash screen streaming fixed

--- playbacks/players/PlayerFactory.js
+++ playbacks/players/PlayerFactory.js
@@ -1,6 +1,6 @@
 import { LLHLSPlayer } from "./LLHLSPlayer.js";
 import { DASHPlayer } from "./DASHPlayer.js";
-// import { WebRTCPlayer } from "./WebRTCPlayer.js";
+
 
 export class PlayerFactory {
   static createPlayer(type, videoElement) {
@@ -9,8 +9,6 @@ export class PlayerFactory {
         return new LLHLSPlayer(videoElement);
       case "dash":
         return new DASHPlayer(videoElement);
-      // case "webrtc":
-      //   return new WebRTCPlayer(videoElement);
       default:
         throw new Error("Unknown player type: " + type);
     }
